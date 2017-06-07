54ccd20b27da396c80e585eba57584bf141e576f
MulaRakesh Reddy <GS-1410@GSLAB.COM>
2017-06-07 02:47:44 -0500

    just-average

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {BrowserAnimationsModule, NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterialModule} from '@angular/material';
 import { AppComponent }  from './app.component';
-
+import 'hammerjs';
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule , BrowserAnimationsModule , NoopAnimationsModule , MaterialModule],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
